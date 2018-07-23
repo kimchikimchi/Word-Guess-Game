69be7d7357d2471c2f4c5f8148c03af037f54581
Jay Cha <kimchikimchi@gmail.com>
2018-07-23 15:52:40 -0400

    - Lifted a sampe page from BootStrap sample as a starting point in   index.html - Added list of predefined target words and randomize chosen one.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,17 @@
+var words = ['madonna', 'michael jackson', 'george michael',
+              'queen', 'prince', 'whitney houston', 'wham',
+              'cyndi lauper', 'annie lennox', 'phil collins',
+              'ac dc', 'bee gees', 'pink floyd', 'dire straits',
+              'scorpions', 'tina turner', 'david bowie'
+            ];
+var tryRemains = 10;
+var userKeyEntered = "";
+var chosenWord = "";
+
+// Randomize word chosen.
+chosenWord = words[ Math.floor(Math.random() * words.length) ] ;
+console.log("Word Chosen is " + chosenWord);
+
 // Handle event after each key is pressed
 document.onkeyup = function(event) {
 
