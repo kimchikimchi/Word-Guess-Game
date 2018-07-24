424ca170836bcd61760977b46659d2d23c4900fe
Jay Cha <kimchikimchi@gmail.com>
2018-07-24 16:43:45 -0400

    - Refactor user facing blank word redraw as helper function   not to repeat myself. - Added logic to remember correct letters guessed. - Added 'game over' display

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,9 +6,38 @@ var words = ['madonna', 'michael jackson', 'george michael',
             ];
 var tryRemains = 10;
 var userKeyEntered = "";
-var chosenWord = "";
-var triedLetters = "";
+var chosenWord = "";            // computer selects this randomly from list.
+var triedLetters = "";          // collect failed, tried letters.
+var matchedLetterIndices = [];
 
+
+/*
+    Helper function.
+
+    Draw user displayed blanks. For any known letters, draw them.
+    Otherwise, leave them as _
+
+    Function is called first when page loaded then every time key is pressed.
+*/
+
+function drawGuessword () {
+    // Redraw to null.  Otherwise, it will concatenate to the last call
+    guessword.textContent = '';
+
+    for (var index = 0; index < chosenWord.length; index++) {
+        if (chosenWord[index] !== ' ') {
+            if ( matchedLetterIndices.includes(index) ) {
+                guessword.textContent += chosenWord[index] + ' ';
+            } else {
+                guessword.textContent += '_ ';
+            }
+
+        } else {
+            // ****** ToDo: How to put space instead of the ugly below. *******
+            guessword.textContent += '-';
+        }
+    }
+}
 
 // Handle event after each key is pressed
 document.onkeyup = function(event) {
@@ -23,14 +52,31 @@ document.onkeyup = function(event) {
 
     // check the entered letter against the randomly selected word
     if (chosenWord.match(keyEntered)) {
+        var index = 0;
+        var matchedIndex = 0;
+
         console.debug("Matched");
-        // If the letter is in the word, change _ into the letter.
-        // To Do:
+
+        // The following returns the index of matching letter.
+        // Returns -1 if it exhausts the sample.
+        while (matchedIndex !== -1) {
+            matchedIndex = chosenWord.indexOf(keyEntered, index);
+
+            if (matchedIndex !== -1) {
+                matchedLetterIndices.push(matchedIndex);
+            }
+            index = matchedIndex + 1;
+        }
+
+        console.debug(matchedLetterIndices);
+        drawGuessword();
+
     } else {
         // If character is already entered, don't count it torward retries.
         if (triedLetters.match(keyEntered)) {
             console.debug("Already entered key: " + keyEntered);
         } else {
+            
             // If the letter is not, lower the retries by one and add to the guessed letter list.
             guessed_letters.textContent += keyEntered + " ";
             triedLetters += keyEntered;
@@ -38,7 +84,10 @@ document.onkeyup = function(event) {
             tryRemains--;
             retries.textContent = tryRemains;
 
-        // If retries are exhausted, game over.
+            // If retries are exhausted, game over.
+            if (tryRemains === 0) {
+                retries.textContent = "GAME OVER!";
+            }
         }
     }
 }
@@ -46,18 +95,9 @@ document.onkeyup = function(event) {
 // Since we are not using jquery here. . .
 document.addEventListener("DOMContentLoaded", function(event) {
     // Randomize word chosen.
-    chosenWord = words[ Math.floor(Math.random() * words.length) ] ;
-    guessword.textContent = '';
+    chosenWord = words[ Math.floor(Math.random() * words.length) ];
     console.log("Word Chosen is " + chosenWord);
-
-    for (var index = 0; index < chosenWord.length; index++) {
-        if (chosenWord[index] !== ' ') {
-            guessword.textContent += '_ ';
-        } else {
-            // ToDo: How to put space instead of the ugly below.
-            guessword.textContent += '(space)';
-        }
-    }
-
     retries.textContent = tryRemains;
+
+    drawGuessword();
 });
