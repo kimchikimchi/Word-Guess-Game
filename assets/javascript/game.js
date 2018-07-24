49add8793b1c99190d5381fbe21b317d6e4268e9
Jay Cha <kimchikimchi@gmail.com>
2018-07-23 23:39:05 -0400

    - Replacing movie file with a screenshot for reduced footprint. - Adding functionalities for word generation, key checking, etc.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,14 +7,57 @@ var words = ['madonna', 'michael jackson', 'george michael',
 var tryRemains = 10;
 var userKeyEntered = "";
 var chosenWord = "";
+var triedLetters = "";
 
-// Randomize word chosen.
-chosenWord = words[ Math.floor(Math.random() * words.length) ] ;
-console.log("Word Chosen is " + chosenWord);
 
 // Handle event after each key is pressed
 document.onkeyup = function(event) {
+    var keyEntered = event.key.toLowerCase();
+    console.debug("You pressed : " + keyEntered);
 
+    // No op if entered key is non-alphabet char
+    if (! keyEntered.match(/^[a-z]$/)) {
+        console.debug("You pressed an non-alphabet char");
+        return;
+    }
 
+    // check the entered letter against the randomly selected word
+    if (chosenWord.match(keyEntered)) {
+        console.debug("Matched");
+        // If the letter is in the word, change _ into the letter.
+        // To Do:
+    } else {
+        // If character is already entered, don't count it torward retries.
+        if (triedLetters.match(keyEntered)) {
+            console.debug("Already entered key: " + keyEntered);
+        } else {
+            // If the letter is not, lower the retries by one and add to the guessed letter list.
+            guessed_letters.textContent += keyEntered + " ";
+            triedLetters += keyEntered;
 
+            tryRemains--;
+            retries.textContent = tryRemains;
+
+        // If retries are exhausted, game over.
+        }
+    }
 }
+
+// Since we are not using jquery here. . .
+document.addEventListener("DOMContentLoaded", function(event) {
+    // Randomize word chosen.
+    chosenWord = words[ Math.floor(Math.random() * words.length) ] ;
+    guessword.textContent = '';
+    console.log("Word Chosen is " + chosenWord);
+
+    for (var index = 0; index < chosenWord.length; index++) {
+        if (chosenWord[index] !== ' ') {
+            guessword.textContent += '_ ';
+        } else {
+            // ToDo: How to put space instead of the ugly below.
+            guessword.textContent += '(space)';
+        }
+    }
+
+    retries.textContent = tryRemains;
+});
