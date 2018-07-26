ce6e25c0aea0097244c19efb4dd533d33f36d971
Jay Cha <kimchikimchi@gmail.com>
2018-07-26 15:17:39 -0400

    - Refactored javascript code into functions for improved readibility. - Styled the HTML page and graphics. - Added instruction to README

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Word-Guess-Game
 Week 3 Homework
+
+- The main page (index.html) displays basic header graphic and text
+based instruction.
+- Predefined words are chosen which are the 80's musician band names.
+- The name will be selected upon page loading and displayed in underscores.  If there is a space in the name, it will be shown as dash ('-').
+
+- User gets 10 guesses each game.
+- If the guess is correct, the matching letter(s) will be displayed.
+- If the guess is wrong, the wrong guess is shown and user loses a retry.
+- When all retries are exhauted, you lose the game.  Game restarts
+- When the user guesses all letters, you will the game.  Game restarts.
+
+ 
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,11 +4,11 @@ var words = ['madonna', 'michael jackson', 'george michael',
               'ac dc', 'bee gees', 'pink floyd', 'dire straits',
               'scorpions', 'tina turner', 'david bowie'
             ];
-var tryRemains = 10;
-var userKeyEntered = "";
-var chosenWord = "";            // computer selects this randomly from list.
-var triedLetters = "";          // collect failed, tried letters.
-var matchedLetterIndices = [];
+var tryRemains;             // Number of guesses remaining.
+var userKeyEntered;         // Letter user enters
+var chosenWord;             // computer selects this randomly from list above.
+var triedLetters;           // collect failed, tried letters.
+var matchedLetterIndices;   // stores array indices of correctly guessed ones.
 
 
 /*
@@ -20,9 +20,22 @@ var matchedLetterIndices = [];
     Function is called first when page loaded then every time key is pressed.
 */
 
+function resetGame() {
+    tryRemains = 10;
+    userKeyEntered = "";
+    chosenWord = "";
+    triedLetters = "";
+    matchedLetterIndices = [];
+
+    chosenWord = words[ Math.floor(Math.random() * words.length) ];
+    console.log("Word Chosen is " + chosenWord);
+    retries.textContent = tryRemains;
+    guessed_letters.textContent = '';
+}
+
 function drawGuessword () {
-    // Redraw to null.  Otherwise, it will concatenate to the last call
     guessword.textContent = '';
+    // Redraw to null.  Otherwise, it will concatenate to the last call
 
     for (var index = 0; index < chosenWord.length; index++) {
         if (chosenWord[index] !== ' ') {
@@ -39,6 +52,49 @@ function drawGuessword () {
     }
 }
 
+function matchLetters(char) {
+    var index = 0;
+    var matchedIndex = 0;
+
+    // The following returns the index of matching letter.
+    // Returns -1 if it exhausts the sample.
+    while (matchedIndex !== -1) {
+        matchedIndex = chosenWord.indexOf(char, index);
+
+        if (matchedIndex !== -1) {
+            matchedLetterIndices.push(matchedIndex);
+        }
+        index = matchedIndex + 1;
+    }
+}
+
+
+function checkGameOver() {
+    //// When there are no more _ in the display, game is over.
+    if (! guessword.textContent.match('_') ) {
+        alert("\t\t\t\t\t\tYou won!");
+        resetGame();
+        drawGuessword();
+        return;
+    }
+
+    // If retries are exhausted, game over.
+    if (tryRemains === 0) {
+        alert("\t\t\t\t\t\tGAME OVER");
+        resetGame();
+        drawGuessword();
+    }
+}
+
+function drawWrongGuess(char) {
+    // If the letter is not, lower the retries by one and add to the guessed letter list
+        guessed_letters.textContent += char + " ";
+        triedLetters += char;
+
+        tryRemains--;
+        retries.textContent = tryRemains;
+}
+
 // Handle event after each key is pressed
 document.onkeyup = function(event) {
     var keyEntered = event.key.toLowerCase();
@@ -52,52 +108,31 @@ document.onkeyup = function(event) {
 
     // check the entered letter against the randomly selected word
     if (chosenWord.match(keyEntered)) {
-        var index = 0;
-        var matchedIndex = 0;
-
         console.debug("Matched");
-
-        // The following returns the index of matching letter.
-        // Returns -1 if it exhausts the sample.
-        while (matchedIndex !== -1) {
-            matchedIndex = chosenWord.indexOf(keyEntered, index);
-
-            if (matchedIndex !== -1) {
-                matchedLetterIndices.push(matchedIndex);
-            }
-            index = matchedIndex + 1;
-        }
+        matchLetters(keyEntered);
 
         console.debug(matchedLetterIndices);
         drawGuessword();
-
     } else {
         // If character is already entered, don't count it torward retries.
         if (triedLetters.match(keyEntered)) {
             console.debug("Already entered key: " + keyEntered);
         } else {
-            
-            // If the letter is not, lower the retries by one and add to the guessed letter list.
-            guessed_letters.textContent += keyEntered + " ";
-            triedLetters += keyEntered;
-
-            tryRemains--;
-            retries.textContent = tryRemains;
-
-            // If retries are exhausted, game over.
-            if (tryRemains === 0) {
-                retries.textContent = "GAME OVER!";
-            }
+            drawWrongGuess(keyEntered);
         }
     }
+
+    checkGameOver();
 }
 
 // Since we are not using jquery here. . .
 document.addEventListener("DOMContentLoaded", function(event) {
     // Randomize word chosen.
+    /*
     chosenWord = words[ Math.floor(Math.random() * words.length) ];
     console.log("Word Chosen is " + chosenWord);
     retries.textContent = tryRemains;
-
+    */
+    resetGame();
     drawGuessword();
 });
